6be4084b0a3e18bf06acb0b8249128117928ce26
Elmer <elmerpf@gmail.com>
2026-06-04 12:50:56 -0300

    noisepower_analytics_funcs_1.2

--- noisepower/noisepowerfuncs.py
+++ noisepower/noisepowerfuncs.py
@@ -3,40 +3,40 @@ import numpy as np
 # Physical constant
 q = 1.602176634e-19 # Electron charge [C]
 
-def calc_S_th(N_0, f_array):
+def calc_S_th(N_0, fq_array):
     """
-    Calculate the thermal noise PSD, assumed constant across frequency.
+    Calculates the PSD of thermal noise, assumed to be constant across frequency.
     Based on S_th = N_0 / 2.
     """
-    return np.full_like(f_array, N_0 / 2)
+    return np.full_like(fq_array, N_0 / 2)
 
-def calc_S_shot(P_RX, G, F, R, f_array):
+def calc_S_shot(P_RX, G, F, R, fq_array):
     """
-    Calculate the shot noise PSD.
-    Assuming k_shot = G^2 * F * q / R.
+    Calculates the PSD of shot noise.
+    Assuming k_shot = (G^2 * F * q )/ R.
     """
     k_shot = (G**2 * F * q) / R
-    S_shot_val = k_shot * P_RX
-    return np.full_like(f_array, S_shot_val)
+    S_shot = k_shot * P_RX
+    return np.full_like(fq_array, S_shot)
 
 def calc_S_RIN(P_TX_sq_avg, RIN_coeff_dB):
     """
-    Calculate the RIN PSD at the source.
+    Calculates the PSD of RIN at the source.
     k_RIN = RIN_coeff_linear / 2.
-    Returns a scalar because the channel filter will be applied in the main function.
+    Returns a scalar, as the channel filter will be applied in the main function.
     """
     # Convert from dB/Hz to linear scale
     RIN_coeff_lin = 10**(RIN_coeff_dB / 10)
     k_RIN = RIN_coeff_lin / 2
     return k_RIN * P_TX_sq_avg
 
-def calc_S_ADC(PAPR, sigma_x_sq, ENOB, fs, f_array):
+def calc_S_ADC(PAPR, sigma_x_sq, ENOB, fs, fq_array):
     """
     Calculates the PSD of ADC quantization noise.
     Based on the quantization noise model.
     """
     S_ADC = ((PAPR**-2) * sigma_x_sq / (12 * fs) * (2**(2 * ENOB - 2)))
-    return np.full_like(f_array, S_ADC)
+    return np.full_like(fq_array, S_ADC)
 
 def calc_S_N(S_RIN_scalar, S_shot_array, S_th_array, S_ADC_array, H_ch_sq_array):
     """
